cccc7f3a90d1e6f2fd2c15f9dee585c2cfdc0d32
ToumaKazusa <gaofc410@gmail.com>
2016-03-01 23:55:05 -0800

    Update Motion.cpp

--- Motion.cpp
+++ Motion.cpp
@@ -163,7 +163,7 @@ degree Cameradegree (Mat Previmg, Mat Currimg, int Height, int Width){
 	Smoofimg(Currimg, 5, Width, Height);
 	int i, j;
 	int thershold = 20;
-	Mat Binary = Mat::zeros(int Height, int Width, int);
+	Mat Binary = Mat::zeros(Height, Width, int);
 	for(i = std::max(0, motion.ydir);i < std::min(Height, Height + motion.xdir);i ++)
 	{
 		for(j = std::max(0, motion.xdir);j < std::min(Width, Width + motion.xdir);j ++)
